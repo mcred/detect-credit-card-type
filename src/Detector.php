0f7aa756d0e220afde8f4921a0190cf975a85b85
Derek Smart <derek@grindaga.com>
2017-06-30 21:42:55 -0400

    can detect amex and discover
    Signed-off-by: Derek Smart <derek@payscape.com>

--- src/Detector.php
+++ src/Detector.php
@@ -13,11 +13,23 @@ class Detector
         return preg_match("/^5$|^5[1-5][0-9]{0,14}$/i", $card);
     }
 
+    private function isAmex(string $card) : bool
+    {
+        return preg_match("/^3$|^3[47][0-9]{0,13}$/i", $card);
+    }
+
+    private function isDiscover(string $card) : bool
+    {
+        return preg_match("/^6$|^6[05]$|^601[1]?$|^65[0-9][0-9]?$|^6(?:011|5[0-9]{2})[0-9]{0,12}$/i", $card);
+    }
+
     public function detect(string $card) : string
     {
         $cardTypes = [
             'Visa',
-            'MasterCard'
+            'MasterCard',
+            'Amex',
+            'Discover'
         ];
         foreach ($cardTypes as $cardType) {
             $method = 'is' . $cardType;
