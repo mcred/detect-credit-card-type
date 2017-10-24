cc3bd2f64fa0813f1d0eb0556bddf9ec76f64b77
Derek Smart <derek@grindaga.com>
2017-10-24 12:42:05 -0400

    JCB test passed
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/Detector.php
+++ src/Detector.php
@@ -23,13 +23,19 @@ class Detector
         return preg_match("/^6$|^6[05]$|^601[1]?$|^65[0-9][0-9]?$|^6(?:011|5[0-9]{2})[0-9]{0,12}$/i", $card);
     }
 
+    private function isJCB(string $card) : bool
+    {
+        return preg_match("/^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/i", $card);
+    }
+
     public function detect(string $card) : string
     {
         $cardTypes = [
             'Visa',
             'Amex',
             'MasterCard',
-            'Discover'
+            'Discover',
+            'JCB'
         ];
         foreach ($cardTypes as $cardType) {
             $method = 'is' . $cardType;
